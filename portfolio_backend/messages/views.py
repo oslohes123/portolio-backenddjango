2721fc37a2664b797c9608eed176d02e6403a2f0
Ashley <ashleytyagi@hotmail.com>
2023-06-12 06:28:00 +0100

    Building out application

---
+++ portfolio_backend/messages/views.py
@@ -0,0 +1,10 @@
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+# Create your views here.
+# Create your tests here.
+class testingView(APIView):
+    def get(self, request):
+        return Response('Testing success', status=status.HTTP_200_OK)
